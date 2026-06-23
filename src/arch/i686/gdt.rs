6e52d2b808cfc776c81bf17633cfda6f0605b57d
auritron <myself.anath@gmail.com>
2026-06-23 20:59:24 +0530

    gdt.rs reorganization

--- src/arch/i686/gdt.rs
+++ src/arch/i686/gdt.rs
@@ -1,4 +1,6 @@
-#[repr(C)]
+use core::arch;
+
+#[repr(C, packed)]
 struct GlobalDescriptor {
     null: GDTEntry,
     kernel_code: GDTEntry,
@@ -7,20 +9,26 @@ struct GlobalDescriptor {
     user_data: GDTEntry,
 }
 
+#[repr(C, packed)]
+struct GDTPointer {
+    limit: u16,
+    base: u32,
+}
+
 struct SegmentType {
-    A: bool,    // access
-    RW: bool,   // read/write
-    DC: bool,   // direction/conforming
-    E: bool,    // executable
+    A: bool,    // access (40)
+    RW: bool,   // read/write (41),
+    DC: bool,   // direction/conforming (42)
+    E: bool,    // executable (43)
 }
 
 #[derive(Debug, Clone, Copy)]
 #[repr(u8)]
 enum AccessLevel {
-    KernelMode,
-    Ring1Mode, // generally not used
-    Ring2Mode, // generally not used
-    UserMode,
+    KernelMode = 0,
+    Ring1Mode = 1, // generally not used
+    Ring2Mode = 2, // generally not used
+    UserMode = 3,
 }
 
 #[repr(C, packed)]
@@ -33,47 +41,57 @@ struct GDTEntry {
     h_base: u8,
 }
 
-#[repr(C, packed)]
-struct GDTPointer {
-    limit: u16,
-    base: u32,
-}
-
 impl GlobalDescriptor {
     
-    fn set_gdt () {
+    fn initialize () {
 
     }
 
-    fn set_gate(
-        &self,
-        limit: u32,
-        base: u32,
-        seg_type: SegmentType,
-        desc_type: bool,
-        desc_access: AccessLevel,
-        present: bool,
-
-        granularity: bool,
-    ) {
-
+    #[unsafe(naked)]
+    fn flush() {
+        unsafe {
+            arch::naked_asm!(
+                
+            );
+        }
     }
 
 }
 
-macro_rules! encode_gate {
-    ($seg:ident, $limit:expr, $base:expr, $access:expr, $gran:expr) => {
-        {
-            $seg.l_limit = $limit && 0xFFFF;
-            $seg.l_base = $base && 0xFFFF;
-            $seg.m_base = ($base >> 16) && 0xFF;
-            $seg.access = $access;
-            $seg.gran = ($limit >> 16) & 0x0F;
-            $seg.gran |= $gran & 0xF0;
-            $seg.h_base = (base >> 24) && 0xFF;
+impl GDTEntry {
+
+    fn set_gate(
+        limit: u32,                 // 0-15, 48-51
+        base: u32,                  // 16-31, 32-39, 56-63
+        seg_type: SegmentType,      // 40-43
+        desc_type: bool,            // 44
+        desc_access: AccessLevel,   // 45-46
+        present: bool,              // 47
+        default_size: bool,         // 54
+        granularity: bool,          // 55
+        //bit 52 (AVL) remains unused, bit 53 (A) is always 0 on 32-bit
+    ) -> Self {
+        Self {
+            l_limit: (limit & 0xFFFF) as u16,
+            l_base: (base & 0xFFFF) as u16,
+            m_base: ((base >> 16) & 0xFF) as u8,
+            access: 
+                (seg_type.A as u8 | (seg_type.RW as u8) << 1 | (seg_type.DC as u8) << 2 | (seg_type.E as u8) << 3) | 
+                ((desc_type as u8) << 4 | (desc_access as u8 & 0x3) << 5 | (present as u8) << 7),
+            gran: ((limit >> 16) & 0xF) as u8 | (default_size as u8) << 6 | (granularity as u8) << 7,
+            h_base: ((base >> 24) & 0xFF) as u8,
         }
-    };
-    ($($invalid:tt)*) => {
-        compile_error!("Invalid use of encode_gate!");
-    };
+    }
+    
+    fn set_from_hex(&mut self, val: u64) -> Self {
+        Self {
+            l_limit: (val & 0xFFFF) as u16,
+            l_base: ((val >> 16) & 0xFFFF) as u16,
+            m_base: ((val >> 32) & 0xFF) as u8,
+            access: ((val >> 40) & 0xFF) as u8,
+            gran: ((val >> 48) & 0xFF) as u8,
+            h_base: ((val >> 56) & 0xFF) as u8,
+        }
+    }
+
 }
